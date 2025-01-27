135dadc6fdbadf1e0761e601a614b56637db4501
kelly-keating <keating.kel@gmail.com>
2025-01-27 15:15:14 +1300

    update theme tests

--- src/components/utilities/ThemeSetter/ThemeSetter.test.tsx
+++ src/components/utilities/ThemeSetter/ThemeSetter.test.tsx
@@ -4,39 +4,53 @@ import { render } from '@testing-library/react';
 import ThemeSetter from './ThemeSetter.tsx';
 
 describe('ThemeSetter is passed a new theme', () => {
-  it('correctly sets primary color', () => {
-    render(<ThemeSetter theme={{ primaryColor: 'red' }} />);
+  it('correctly sets main background color', () => {
+    render(<ThemeSetter theme={{ bgMain: 'red' }} />);
     const root = document.documentElement;
 
-    expect(root).toHaveStyle({ '--theme-color-base': 'red' });
+    expect(root).toHaveStyle({ '--theme-bg-main': 'red' });
   });
 
-  it('correctly sets secondary color', () => {
-    render(<ThemeSetter theme={{ secondaryColor: 'blue' }} />);
+  it('correctly sets light background color', () => {
+    render(<ThemeSetter theme={{ bgLight: 'blue' }} />);
     const root = document.documentElement;
 
-    expect(root).toHaveStyle({ '--theme-color-secondary': 'blue' });
+    expect(root).toHaveStyle({ '--theme-bg-light': 'blue' });
+  });
+
+  it('correctly sets dark background color', () => {
+    render(<ThemeSetter theme={{ bgDark: 'green' }} />);
+    const root = document.documentElement;
+
+    expect(root).toHaveStyle({ '--theme-bg-dark': 'green' });
+  });
+
+  it('correctly sets light text color', () => {
+    render(<ThemeSetter theme={{ textLight: 'purple' }} />);
+    const root = document.documentElement;
+
+    expect(root).toHaveStyle({ '--theme-text-light': 'purple' });
+  });
+
+  it('correctly sets dark text color', () => {
+    render(<ThemeSetter theme={{ textDark: 'orange' }} />);
+    const root = document.documentElement;
+
+    expect(root).toHaveStyle({ '--theme-text-dark': 'orange' });
   });
 
   it('correctly sets highlight color', () => {
-    render(<ThemeSetter theme={{ highlightColor: 'green' }} />);
+    render(<ThemeSetter theme={{ highlight: 'yellow' }} />);
     const root = document.documentElement;
 
-    expect(root).toHaveStyle({ '--theme-color-highlight': 'green' });
+    expect(root).toHaveStyle({ '--theme-highlight': 'yellow' });
   });
 
-  it('correctly sets text color', () => {
-    render(<ThemeSetter theme={{ textColor: 'purple' }} />);
+  it('correctly sets alternative highlight color', () => {
+    render(<ThemeSetter theme={{ highlightAlternative: 'pink' }} />);
     const root = document.documentElement;
 
-    expect(root).toHaveStyle({ '--theme-text-color': 'purple' });
-  });
-
-  it('correctly sets alternate text color', () => {
-    render(<ThemeSetter theme={{ textAltColor: 'orange' }} />);
-    const root = document.documentElement;
-
-    expect(root).toHaveStyle({ '--theme-text-alt': 'orange' });
+    expect(root).toHaveStyle({ '--theme-highlight-alt': 'pink' });
   });
 });
 
@@ -45,50 +59,46 @@ describe('ThemeSetter is passed multiple new values', () => {
     render(
       <ThemeSetter
         theme={{
-          primaryColor: 'red',
-          secondaryColor: 'blue',
-          highlightColor: 'green',
-          textColor: 'purple',
-          textAltColor: 'orange',
+          bgMain: 'red',
+          bgDark: 'blue',
+          highlight: 'green',
+          textLight: 'purple',
+          textDark: 'orange',
         }}
       />
     );
     const root = document.documentElement;
 
     expect(root).toHaveStyle({
-      '--theme-color-base': 'red',
-      '--theme-color-secondary': 'blue',
-      '--theme-color-highlight': 'green',
-      '--theme-text-color': 'purple',
-      '--theme-text-alt': 'orange',
+      '--theme-bg-main': 'red',
+      '--theme-bg-dark': 'blue',
+      '--theme-highlight': 'green',
+      '--theme-text-light': 'purple',
+      '--theme-text-dark': 'orange',
     });
   });
 });
 
 describe('ThemeSetter is passed an updated theme', () => {
   it('correctly updates outdated values', () => {
-    const { rerender } = render(
-      <ThemeSetter theme={{ primaryColor: 'red' }} />
-    );
+    const { rerender } = render(<ThemeSetter theme={{ bgMain: 'red' }} />);
     const root = document.documentElement;
 
-    rerender(<ThemeSetter theme={{ primaryColor: 'blue' }} />);
-    expect(root).toHaveStyle({ '--theme-color-base': 'blue' });
+    rerender(<ThemeSetter theme={{ bgMain: 'blue' }} />);
+    expect(root).toHaveStyle({ '--theme-bg-main': 'blue' });
   });
 
   it('add new values and does not update unchanged values', () => {
     const { rerender } = render(
-      <ThemeSetter theme={{ primaryColor: 'red', secondaryColor: 'red' }} />
+      <ThemeSetter theme={{ bgMain: 'red', bgDark: 'red' }} />
     );
     const root = document.documentElement;
 
-    rerender(
-      <ThemeSetter theme={{ secondaryColor: 'blue', highlightColor: 'blue' }} />
-    );
+    rerender(<ThemeSetter theme={{ bgDark: 'blue', highlight: 'blue' }} />);
     expect(root).toHaveStyle({
-      '--theme-color-base': 'red',
-      '--theme-color-secondary': 'blue',
-      '--theme-color-highlight': 'blue',
+      '--theme-bg-main': 'red',
+      '--theme-bg-dark': 'blue',
+      '--theme-highlight': 'blue',
     });
   });
 });
